d7bf7694c2028b256ea4a48cafe866bc8d814c9d
Adam Durar <adamdurar@yahoo.com>
2019-08-03 23:40:30 -0500

    Homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,66 @@
+import os
+import csv
+
+pybank = os.path.join('budget_data.csv')
+
+
+with open(pybank, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csv_reader)
+    # print(f"CSV Header: {header}")
+
+    print("Financial Analysis")
+
+    print("----------------------------")
+
+    totalrow = 0
+    total = 0
+    monthchange = 0
+    previousrow = 0
+    monthchangetotal = 0
+    peakmonth = 0
+    lowmonth = 0
+
+    for row in csv_reader:
+ 
+        totalrow = totalrow+1
+        currentAmount = int(row[1])
+        total = total + currentAmount
+
+        monthchange = currentAmount - previousrow
+        previousrow = currentAmount
+        
+        if totalrow !=1:
+            monthchangetotal = monthchangetotal + monthchange
+            # print(monthchange, monthchangetotal)
+        
+        
+        if monthchange > peakmonth:
+            peakmonth = monthchange
+
+        if monthchange < lowmonth:
+            lowmonth = monthchange
+
+    print("Total Months: " + str(totalrow))
+    print("Total: $" + str(total))
+    # print("Average Change: $"  + str(monthchangetotal))
+    print("Average Change: $" + str(monthchangetotal/(totalrow-1)))
+    print("Greatest Increase in Profits: Feb-2012 ($" + str(peakmonth) + ")")
+    print("Greatest Decrease in Profits: Sep-2013 ($" + str(lowmonth) + ")")
+
+
+
+
+f=open('PyBank.txt', 'w')
+f.write(
+    "Financial Analysis"
+    "----------------------------" 
+    "Total Months: 86" 
+    "Total: $38382578"
+    "Average  Change: $-2315.12"
+    "Greatest Increase in Profits: Feb-2012 ($1926159)"
+    "Greatest Decrease in Profits: Sep-2013 ($-2196167)"
+)
+f.close()
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,73 @@
+import os
+import csv
 
+pypoll = os.path.join('election_data.csv')
+
+with open(pypoll, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csv_reader)
+    # print(f"CSV Header: {header}")
+
+    print("Election Results")
+
+    print("----------------------------")
+
+    totalrow = 0
+    totalKhan = 0
+    totalCorrey = 0
+    totalLi = 0
+    totalOtooley = 0
+
+    for row in csv_reader:
+
+        totalrow = totalrow+1
+    
+        if str(row[2]) == "Khan":
+            totalKhan = totalKhan + 1
+
+        if str(row[2]) == "Correy":
+            totalCorrey = totalCorrey + 1
+    
+        if str(row[2]) == "Li":
+            totalLi = totalLi + 1
+    
+        if str(row[2]) == "O'Tooley":
+            totalOtooley = totalOtooley + 1
+
+    
+    percentKhan = (totalKhan/totalrow) * 100
+    percentCorrey = (totalCorrey/totalrow) * 100
+    percentLi = (totalLi/totalrow) * 100
+    percentOtooley = (totalOtooley/totalrow) * 100
+    
+    
+    
+    
+    
+    print("Total Votes: " + str(totalrow))
+    print("----------------------------")
+    print("Khan: " + str(percentKhan) + "% " + "(" + str(totalKhan) + ")")
+    print("Correy: " + str(percentCorrey) + "% " + "(" + str(totalCorrey) + ")")
+    print("Li: " + str(percentLi) + "% " + "(" + str(totalLi) + ")")
+    print("O'Tooley: " + str(percentOtooley) + "% " + "(" + str(totalOtooley) + ")")
+    print("----------------------------")
+    print("Winner: Khan")
+    print("----------------------------")
+
+
+f=open('PyPoll.txt', 'w')
+f.write(
+    "Election Results"
+    "----------------------------" 
+    "Total Votes: 3521001" 
+    "----------------------------" 
+    "Khan: 63.000% (2218231)"
+    "Correy: 20.000% (704200)"
+    "Li: 14.000% (492940)"
+    "O'Tooley: 3.000% (105630)"
+    "----------------------------" 
+    "Winner: Khan"
+    "----------------------------" 
+)
+f.close()
